79964ac875eee66cb07cf549cde5a590c9f79b95
le1nux <le1nux@users.noreply.github.com>
2016-03-16 00:39:04 +0100

    each schedule has its own scheduleRunnerId now.

--- TestingFramework/src/com/lue/common/Schedule.java
+++ TestingFramework/src/com/lue/common/Schedule.java
@@ -14,100 +14,111 @@ import com.lue.client.tests.TestParameters;
 
 @XmlRootElement(namespace = "com.lue.server;")
 public class Schedule implements Iterable<Schedule.ScheduleElement>, Serializable{
-    private static final long serialVersionUID = 1L;
-    @XmlElement
-    protected List<ScheduleElement> schedule;
-
-    public Schedule() {
-	schedule = new ArrayList<ScheduleElement>();
-    }
-
-    public void addScheduleElement (ScheduleElement test) {
-	schedule.add(test);
-    }
-    
-    public List<ScheduleElement> getSchedule() {
-        return schedule;
-    }
-
-    public ScheduleElement getScheduleElement(int testId) {
-	if(schedule.size() > testId) 
-	    return schedule.get(testId);
-	return null;
-    }
-    
-    @JsonIgnore
-    public int getElementCount() {
-	return schedule.size();
-    }
-
-    @Override
-    public Iterator<ScheduleElement> iterator() {
-	Iterator<ScheduleElement> it = new Iterator<ScheduleElement>() {
-
-	    private int currentIndex = 0;
-
-	    @Override
-	    public boolean hasNext() {
-		return currentIndex < schedule.size();
-	    }
-
-	    @Override
-	    public ScheduleElement next() {
-		return schedule.get(currentIndex++);
-	    }
-
-	    @Override
-	    public void remove() {
-		throw new UnsupportedOperationException();
-	    }
-	};
-	return it;
-    }
-
-    public static class ScheduleElement implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	protected String testKey;
-	protected TestParameters parameters;
-	protected TestResult testResult;
+	protected int scheduleRunnerId;
 
-	public ScheduleElement() {
-	    
-	}
-	
-	public ScheduleElement(String testKey, TestParameters parameters, TestResult testResult) {
-	    this.testKey = testKey;
-	    this.parameters = parameters;
-	    this.testResult = testResult;
+	@XmlElement
+	protected List<ScheduleElement> schedule;
+
+	public int getScheduleRunnerId() {
+		return scheduleRunnerId;
 	}
 
-	public String getTestKey() {
-	    return testKey;
+	public void setScheduleRunnerId(int scheduleRunnerId) {
+		this.scheduleRunnerId = scheduleRunnerId;
 	}
 
-	public void setTestKey(String testKey) {
-	    this.testKey = testKey;
+	public Schedule() {
+		schedule = new ArrayList<ScheduleElement>();
 	}
 
-	public TestParameters getParameters() {
-	    return parameters;
+	public void addScheduleElement (ScheduleElement test) {
+		schedule.add(test);
 	}
 
-	public void setParameters(TestParameters parameters) {
-	    this.parameters = parameters;
+	public List<ScheduleElement> getSchedule() {
+		return schedule;
 	}
 
-	public TestResult getTestResult() {
-	    return testResult;
+	public ScheduleElement getScheduleElement(int testId) {
+		if(schedule.size() > testId) 
+			return schedule.get(testId);
+		return null;
 	}
 
-	public boolean hasTestResult() {
-	    return testResult != null;
+	@JsonIgnore
+	public int getElementCount() {
+		return schedule.size();
 	}
 
-	public void setTestResult(TestResult testResult) {
-	    this.testResult = testResult;
+	@Override
+	public Iterator<ScheduleElement> iterator() {
+		Iterator<ScheduleElement> it = new Iterator<ScheduleElement>() {
+
+			private int currentIndex = 0;
+
+			@Override
+			public boolean hasNext() {
+				return currentIndex < schedule.size();
+			}
+
+			@Override
+			public ScheduleElement next() {
+				return schedule.get(currentIndex++);
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+		return it;
 	}
-    }
+
+	public static class ScheduleElement implements Serializable{
+		private static final long serialVersionUID = 1L;
+
+		protected String testKey;
+		protected TestParameters parameters;
+		protected TestResult testResult;
+
+		public ScheduleElement() {
+
+		}
+
+		public ScheduleElement(String testKey, TestParameters parameters, TestResult testResult) {
+			this.testKey = testKey;
+			this.parameters = parameters;
+			this.testResult = testResult;
+		}
+
+		public String getTestKey() {
+			return testKey;
+		}
+
+		public void setTestKey(String testKey) {
+			this.testKey = testKey;
+		}
+
+		public TestParameters getParameters() {
+			return parameters;
+		}
+
+		public void setParameters(TestParameters parameters) {
+			this.parameters = parameters;
+		}
+
+		public TestResult getTestResult() {
+			return testResult;
+		}
+
+		public boolean hasTestResult() {
+			return testResult != null;
+		}
+
+		public void setTestResult(TestResult testResult) {
+			this.testResult = testResult;
+		}
+	}
 }
