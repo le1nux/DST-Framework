9de27b89bcbbf6ccd1ef08b1873494bf292ef4c2
le1nux <le1nux@users.noreply.github.com>
2016-03-15 19:54:01 +0100

    Added Iterator to ScheduleRunnerStorage

--- TestingFramework/src/com/lue/server/ScheduleRunnerStorage.java
+++ TestingFramework/src/com/lue/server/ScheduleRunnerStorage.java
@@ -3,11 +3,12 @@ package com.lue.server;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.lue.client.ScheduleRunnerIF;
 
-public class ScheduleRunnerStorage {
+public class ScheduleRunnerStorage implements Iterable<ScheduleRunnerIF>{
 	protected ArrayList<ScheduleRunnerIF> scheduleRunners;
 
 	public ScheduleRunnerStorage() {
@@ -58,4 +59,29 @@ public class ScheduleRunnerStorage {
 		}
 		return deadRunners;
 	}
+
+	@Override
+	public Iterator<ScheduleRunnerIF> iterator() {
+		return new StorageIterator();
+	}
+	
+	public class StorageIterator implements Iterator<ScheduleRunnerIF> {
+		int current = -1;
+		
+	    public boolean hasNext() {
+	    	return scheduleRunners.size()-1 > current; 	    
+	    }
+
+	    public ScheduleRunnerIF next() {
+	    	current++;
+	    	return scheduleRunners.get(current);
+	    }
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+		}
+	    
+	}
+
 }
