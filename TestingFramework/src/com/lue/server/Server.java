625bec2b905ea95fff9922a7c58c424df2402cae
le1nux <le1nux@users.noreply.github.com>
2016-03-15 23:36:49 +0100

    Got rid of MaxScheduleRunnerCount

--- TestingFramework/src/com/lue/server/Server.java
+++ TestingFramework/src/com/lue/server/Server.java
@@ -10,67 +10,61 @@ import com.lue.server.controller.Controller;
 import com.lue.server.webinterface.WebServer;
 
 public class Server {
-    private static final int WEB_SERVER_PORT = 8080;
-    private static final int SCHEDULE_RUNNER_COUNT = 1;
-    private static final int REGISTRY_PORT = 1099;
-    private static final String RMI_HOST_NAME = "127.0.0.1";
+	private static final int WEB_SERVER_PORT = 8080;
+	private static final int REGISTRY_PORT = 1099;
+	private static final String RMI_HOST_NAME = "127.0.0.1";
 
-    protected Scheduler scheduler;
-    protected WebServer webServer;
+	protected Scheduler scheduler;
+	protected WebServer webServer;
 
 
-    public Server() {
-	try {
-	    setUpScheduler();
-	    new Controller(scheduler);
-	} catch (RemoteException | AlreadyBoundException e) {
-	    e.printStackTrace();
-	}
-	setUpWebInterface();
-    }
-
-    private void setUpScheduler() throws RemoteException, AlreadyBoundException {
-	// explanation for this dirty hack: http://www.javacodegeeks.com/2013/11/two-things-to-remember-when-using-java-rmi.html
-	System.setProperty("java.rmi.server.hostname", RMI_HOST_NAME);
-	scheduler = new Scheduler(SCHEDULE_RUNNER_COUNT, REGISTRY_PORT);
-	try {
-	    scheduler.setScheduleStorage(ScheduleStorage.generateDebugScheduleStorage(SCHEDULE_RUNNER_COUNT));
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
-    }
-
-    private void setUpWebInterface(){
-	// handler to catch possible lifecycle exception thrown by tomcat at start
-	Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
-	    public void uncaughtException(Thread th, Throwable ex) {
-		System.out.println("Uncaught exception: " + ex);
-		System.exit(1);
-	    }
-	};
-	
-	webServer = new WebServer(WEB_SERVER_PORT, scheduler);
-	Thread t = new Thread(){
-	    @Override
-	    public void run() {
+	public Server() {
 		try {
-		    webServer.startServer();
-		} catch (LifecycleException e) {
-		    e.printStackTrace();
+			setUpScheduler();
+			new Controller(scheduler);
+		} catch (RemoteException | AlreadyBoundException e) {
+			e.printStackTrace();
 		}
-	    }
-	};
-	t.setUncaughtExceptionHandler(h);
-	t.start();
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-	new Server();
-	System.out.println("Server started! Press enter to stop the server...");
-	try {
-	    System.in.read();
-	} catch (IOException e) {
-	    e.printStackTrace();
+		setUpWebInterface();
 	}
-    }
+
+	private void setUpScheduler() throws RemoteException, AlreadyBoundException {
+		// explanation for this dirty hack: http://www.javacodegeeks.com/2013/11/two-things-to-remember-when-using-java-rmi.html
+		System.setProperty("java.rmi.server.hostname", RMI_HOST_NAME);
+		scheduler = new Scheduler(REGISTRY_PORT);
+	}
+
+	private void setUpWebInterface(){
+		// handler to catch possible lifecycle exception thrown by tomcat at start
+		Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
+			public void uncaughtException(Thread th, Throwable ex) {
+				System.out.println("Uncaught exception: " + ex);
+				System.exit(1);
+			}
+		};
+
+		webServer = new WebServer(WEB_SERVER_PORT, scheduler);
+		Thread t = new Thread(){
+			@Override
+			public void run() {
+				try {
+					webServer.startServer();
+				} catch (LifecycleException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		t.setUncaughtExceptionHandler(h);
+		t.start();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new Server();
+		System.out.println("Server started! Press enter to stop the server...");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
