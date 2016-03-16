abd8eb708b0b92bfa8a61fa5ccee17c11518129b
le1nux <le1nux@users.noreply.github.com>
2016-03-16 15:49:29 +0100

    see last commit ...

--- TestingFramework/src/com/lue/server/webinterface/ScheduleRunnersServlet.java
+++ TestingFramework/src/com/lue/server/webinterface/ScheduleRunnersServlet.java
@@ -2,6 +2,7 @@ package com.lue.server.webinterface;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,27 +10,41 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.lue.client.NameIF;
+import com.lue.client.ScheduleRunnerIF;
+import com.lue.common.SupportedTests;
 import com.lue.common.util.JsonProcessor;
 import com.lue.server.SchedulerDataAccessIF;
 
 public class ScheduleRunnersServlet extends ApiServlet{
-    private static final long serialVersionUID = 1L;
-    
-    public ScheduleRunnersServlet(SchedulerDataAccessIF dataAccess) {
-	super(dataAccess);
-    }  
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	response.setContentType("application/json");
-	PrintWriter out = response.getWriter();
-	List<? extends NameIF> scheduleRunners = dataAccess.getScheduleRunners();
-	List<String> scheduleRunnersRep = new ArrayList<>();
-	for(final NameIF client : scheduleRunners){
-	    scheduleRunnersRep.add(client.getName());
+	public ScheduleRunnersServlet(SchedulerDataAccessIF dataAccess) {
+		super(dataAccess);
+	}  
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		List<? extends ScheduleRunnerIF> scheduleRunners = dataAccess.getScheduleRunners();
+		List<Object> scheduleRunnersRep = new ArrayList<>();
+		for(final ScheduleRunnerIF scheduleRunner : scheduleRunners){
+			scheduleRunnersRep.add(new ScheduleRunnerRepresentation(scheduleRunner));
+		}
+		out.println(JsonProcessor.toJson(scheduleRunnersRep));
+		out.close();
+	}  
+
+
+	private class ScheduleRunnerRepresentation{
+		@SuppressWarnings("unused")
+		int scheduleRunnerId;
+		@SuppressWarnings("unused")
+		SupportedTests supportedTests;
+
+		public ScheduleRunnerRepresentation(ScheduleRunnerIF scheduleRunner) throws RemoteException {
+			scheduleRunnerId = scheduleRunner.getId();
+			supportedTests = scheduleRunner.getSupportedTests();
+		}		
 	}
-	out.println(JsonProcessor.toJson(scheduleRunnersRep));
-	out.close();
-    }   
 }
