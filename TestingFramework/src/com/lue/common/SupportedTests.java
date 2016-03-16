fbc7085045fd775635c327c4177b573c74f47375
le1nux <le1nux@users.noreply.github.com>
2016-03-16 15:50:22 +0100

    Fixed tabbing in SupportedTests.

--- TestingFramework/src/com/lue/common/SupportedTests.java
+++ TestingFramework/src/com/lue/common/SupportedTests.java
@@ -8,74 +8,74 @@ import javax.xml.bind.annotation.XmlElement;
 
 
 public class SupportedTests implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @XmlElement
-    protected Map<String, SupportedTest> supportedTestsMap;
-    
-    public SupportedTests() {
-	supportedTestsMap = new HashMap<>();
-    }
-    
-    public void addSupportedTest(SupportedTest supportedTest) {
-	supportedTestsMap.put(supportedTest.name, supportedTest);
-    }
-    
-    public boolean isTestSupported(String key) {
-	return supportedTestsMap.containsKey(key);
-    }
-    
-    public SupportedTest getSupportedTest(String key) {
-	if(supportedTestsMap.containsKey(key))	
-	    return supportedTestsMap.get(key);
-	return null;
-    }
-    
-    public Map<String, SupportedTest>  getSupportedTests() {
-	return supportedTestsMap;	
-    }
-    
-    public void intersectSupportedTests(SupportedTests st) {
-	supportedTestsMap.keySet().retainAll(st.getSupportedTests().keySet());
-    }
-    
-    @Override
-    public String toString() {
-	String rep = "";
-	for(Map.Entry<String, SupportedTest> entry : supportedTestsMap.entrySet()) {
-	    SupportedTest test = entry.getValue();
-	    rep += test.toString() + "\n";
-	}
-	return rep;
-    }
-
-    public static class SupportedTest implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
-	protected String name;
-	
-	public SupportedTest() {
-
-	}
-	
-	public SupportedTest(String name) {
-	    this.name = name;
-	}
-	
-	
 	@XmlElement
-	public String getName() {
-	    return name;
+	protected Map<String, SupportedTest> supportedTestsMap;
+
+	public SupportedTests() {
+		supportedTestsMap = new HashMap<>();
 	}
 
-	public void setName(String name) {
-	    this.name = name;
+	public void addSupportedTest(SupportedTest supportedTest) {
+		supportedTestsMap.put(supportedTest.name, supportedTest);
+	}
+
+	public boolean isTestSupported(String key) {
+		return supportedTestsMap.containsKey(key);
+	}
+
+	public SupportedTest getSupportedTest(String key) {
+		if(supportedTestsMap.containsKey(key))	
+			return supportedTestsMap.get(key);
+		return null;
+	}
+
+	public Map<String, SupportedTest> getSupportedTests() {
+		return supportedTestsMap;	
+	}
+
+	public void intersectSupportedTests(SupportedTests st) {
+		supportedTestsMap.keySet().retainAll(st.getSupportedTests().keySet());
 	}
 
 	@Override
 	public String toString() {
-	    String rep = "";
-	    rep += name + "\n";
-	    return rep;
+		String rep = "";
+		for(Map.Entry<String, SupportedTest> entry : supportedTestsMap.entrySet()) {
+			SupportedTest test = entry.getValue();
+			rep += test.toString() + "\n";
+		}
+		return rep;
 	}
-    }
+
+	public static class SupportedTest implements Serializable{
+		private static final long serialVersionUID = 1L;
+
+		protected String name;
+
+		public SupportedTest() {
+
+		}
+
+		public SupportedTest(String name) {
+			this.name = name;
+		}
+
+
+		@XmlElement
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			String rep = "";
+			rep += name + "\n";
+			return rep;
+		}
+	}
 }
