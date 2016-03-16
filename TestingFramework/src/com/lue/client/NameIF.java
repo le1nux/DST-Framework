c998145ad4515e9e99dd66c0b6c939de1abc67ea
le1nux <le1nux@users.noreply.github.com>
2016-03-16 15:46:28 +0100

    commented out parts of NameIF. Probably not needed anymore.

--- TestingFramework/src/com/lue/client/NameIF.java
+++ TestingFramework/src/com/lue/client/NameIF.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 import org.codehaus.jackson.annotate.JsonValue;
 
 public interface NameIF extends Remote{
-    @JsonValue
-    public String getName() throws RemoteException;
+    //@JsonValue
+    //public String getName() throws RemoteException;
 }
