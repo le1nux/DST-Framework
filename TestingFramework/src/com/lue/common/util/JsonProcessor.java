bf430acfaf2deeeb9c9e1752e8bb096661338f43
le1nux <le1nux@users.noreply.github.com>
2016-03-16 15:46:55 +0100

    fixed tabbing in JsonProcessor

--- TestingFramework/src/com/lue/common/util/JsonProcessor.java
+++ TestingFramework/src/com/lue/common/util/JsonProcessor.java
@@ -13,33 +13,33 @@ import org.codehaus.jackson.map.SerializationConfig;
 
 
 public class JsonProcessor {
-    protected static ObjectMapper mapper;
-    static {
-	mapper = new ObjectMapper();
-	mapper.setVisibility(JsonMethod.FIELD, Visibility.ANY);
-	mapper.enableDefaultTyping();
-	mapper.enable(SerializationConfig.Feature.INDENT_OUTPUT);
-    }
+	protected static ObjectMapper mapper;
+	static {
+		mapper = new ObjectMapper();
+		mapper.setVisibility(JsonMethod.FIELD, Visibility.ANY);
+		mapper.enableDefaultTyping();
+		mapper.enable(SerializationConfig.Feature.INDENT_OUTPUT);
+	}
 
 
-    private JsonProcessor() {   
+	private JsonProcessor() {   
 
-    }
+	}
 
 
-    public static String toJson(Object o) throws JsonGenerationException, JsonMappingException, IOException {
-	return mapper.writeValueAsString(o);
-    }
+	public static String toJson(Object o) throws JsonGenerationException, JsonMappingException, IOException {
+		return mapper.writeValueAsString(o);
+	}
 
-    public static Object fromJson(String o, Class<?> type) throws JsonParseException, JsonMappingException, IOException {
-	return mapper.readValue(o, type);
-    }
-    
-    public static void objectToJsonFile(String path, Object o) throws JsonGenerationException, JsonMappingException, IOException {
-	mapper.writeValue(new File(path), o);
-    }
-    
-    public static Object jsonFileToObject(String path, Class<?> type) throws JsonParseException, JsonMappingException, IOException {
-	return mapper.readValue(new File(path), type);
-    }
+	public static Object fromJson(String o, Class<?> type) throws JsonParseException, JsonMappingException, IOException {
+		return mapper.readValue(o, type);
+	}
+
+	public static void objectToJsonFile(String path, Object o) throws JsonGenerationException, JsonMappingException, IOException {
+		mapper.writeValue(new File(path), o);
+	}
+
+	public static Object jsonFileToObject(String path, Class<?> type) throws JsonParseException, JsonMappingException, IOException {
+		return mapper.readValue(new File(path), type);
+	}
 }
